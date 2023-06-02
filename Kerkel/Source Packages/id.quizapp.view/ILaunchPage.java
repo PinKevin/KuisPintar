669233a1abb0a7ca18446c809f99f1d317d63f6f
Emerio Kevin Aryaputra <emeriokevin2320@gmail.com>
2023-06-02 17:29:08 +0700

    Delete Kerkel directory

--- Kerkel/Source Packages/id.quizapp.view/ILaunchPage.java
+++
@@ -1,8 +0,0 @@
-import java.awt.event.ActionListener;
-
-public interface ILaunchPage {
-    void setActionListener(ActionListener listener);
-    void showMainPage();
-    void showSecondaryPage();
-    void showPreviousPage();
-}
