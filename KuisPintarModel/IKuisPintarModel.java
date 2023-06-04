058dcf3ff6ac25ad4003ee49b16f95407c505094
yusufnajhan <100354817+yusufnajhan@users.noreply.github.com>
2023-06-04 19:53:33 +0700

    Add files via upload

---
+++ KuisPintarModel/IKuisPintarModel.java
@@ -0,0 +1,9 @@
+package KuisPintarModel;
+
+public interface IKuisPintarModel {
+    void generateEquation();
+    int getFirstNumber();
+    int getSecondNumber();
+    String getCurrentOperator();
+    int getAnswerNumber();
+}
